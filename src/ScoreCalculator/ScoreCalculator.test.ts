8942b205293809bd0fb96675570b1c23657ffab8
Axel Friberg <hello@axelfriberg.com>
2020-01-11 11:59:30 +0100

    Implement calculate function and add more tests

--- src/ScoreCalculator/ScoreCalculator.test.ts
+++ src/ScoreCalculator/ScoreCalculator.test.ts
@@ -1,26 +1,100 @@
 import { calculate } from "./ScoreCalculator";
 
 it("Calculates the score of a frame without strike or spare to equal the number of pins knocked down", () => {
-  const historicRolls = [4];
-  expect(calculate(4, historicRolls)).toEqual(8);
+  const rolls = [4, 4];
+  expect(calculate(rolls)).toEqual(8);
+});
+
+it("Calculates the score of two frames with the second frame with only one roll", () => {
+  const rolls = [4, 4, 4];
+  expect(calculate(rolls)).toEqual(8);
 });
 
 it("Calculates the score of a spare to equal 10 plus the number of pins in the next roll", () => {
-  const historicRolls = [4, 6];
-  expect(calculate(4, historicRolls)).toEqual(14);
+  const rolls = [4, 6, 4];
+  expect(calculate(rolls)).toEqual(14);
 });
 
 it("Calculates the score of a strike to equal 10 plus the number of pins in the next two rolls", () => {
-  const historicRolls = [10, 5];
-  expect(calculate(3, historicRolls)).toEqual(19);
+  const rolls = [10, 5, 3];
+  expect(calculate(rolls)).toEqual(26);
 });
 
 it("Calculates the score of a strike, spare, 4|0 to be 38", () => {
-  const historicRolls = [10, 4, 6, 4];
-  expect(calculate(0, historicRolls)).toEqual(38);
+  const rolls = [10, 4, 6, 4, 0];
+  expect(calculate(rolls)).toEqual(38);
+});
+
+it("Calculates the score when you get a spare in the last frame", () => {
+  const rolls = [
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    9,
+    5
+  ];
+  expect(calculate(rolls)).toEqual(33);
+});
+
+it("Calculates the score when you get a strike in the last frame", () => {
+  const rolls = [
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    10,
+    5,
+    5
+  ];
+  expect(calculate(rolls)).toEqual(38);
+});
+
+it("Calculates the score like a spare when you get a zero followed by a 10 roll in a frame", () => {
+  const rolls = [0, 10, 3, 4];
+  expect(calculate(rolls)).toEqual(20);
 });
 
 it("Calculates the score of a 12 strikes to 300", () => {
-  const historicRolls = new Array(11).fill(10);
-  expect(calculate(10, historicRolls)).toEqual(300);
+  const rolls = new Array(12).fill(10);
+  expect(calculate(rolls)).toEqual(300);
+});
+
+it("Calculates the score to zero if you get a spare in the first frame but haven't rolled in the second frame", () => {
+  const rolls = [5, 5];
+  expect(calculate(rolls)).toEqual(0);
+});
+
+it("Calculates the score to zero if you get a strike in the first frame but haven't rolled twice in the second frame", () => {
+  const rolls = [10, 5];
+  expect(calculate(rolls)).toEqual(0);
 });
--- src/ScoreCalculator/ScoreCalculator.ts
+++ src/ScoreCalculator/ScoreCalculator.ts
@@ -1,3 +1,29 @@
-export function calculate(currentRoll: number, historicRolls: number[]): number {
-  return -1;
+export function calculate(rolls: number[]): number {
+  let currentScore = 0;
+  const lastRollIndex = rolls.length - 1;
+
+  if (lastRollIndex < 1) {
+    return currentScore;
+  }
+
+  for (let i = 0; i < rolls.length - 1; i += 2) {
+    const firstRollInFrame = rolls[i];
+    const secondRollInFrame = rolls[i + 1];
+    const numberOfPinsInFrame = firstRollInFrame + secondRollInFrame;
+
+    if (numberOfPinsInFrame < 10) {
+      currentScore += firstRollInFrame + secondRollInFrame;
+    } else if (numberOfPinsInFrame === 10) {
+      if (i + 2 <= lastRollIndex) {
+        currentScore += 10 + rolls[i + 2];
+      }
+    } else {
+      if (i + 2 <= lastRollIndex) {
+        currentScore += 10 + rolls[i + 1] + rolls[i + 2];
+        i -= 1;
+      }
+    }
+  }
+
+  return currentScore;
 }
